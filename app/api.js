0ed807d8a350ea55fc542352f2ce3bbda1264a98
Glitch (brick-board) <none>
2017-11-01 11:11:25 +0000

    Checkpoint

--- app/api.js
+++ app/api.js
@@ -2,20 +2,9 @@ module.exports = function(app,db) {
   
   app.get('/new/:url*', function(req,res){
 
-    if(req.params.url) {
-      urls[shortUrlNumber++] = req.params.url;
-
-      var response = {};
-      response['shortUrl'] = process.env.APP_URL + shortUrlNumber;
-      response['originalUrl'] = urls[shortUrlNumber];
-
-      shortUrlNumber++;
-
-      res.send(response);
+    if(validateUrl(req.params.url)) {
+      
     }
-    else {
-      res.send('Please enter a valid url');
-    } 
   });
 
   app.get('/:url*', processUrl);  
@@ -28,7 +17,6 @@ module.exports = function(app,db) {
   function addNewUrl() {
     
   }
-  
   
   function getUrl(link, db, res) {
     var sites = db.collection('sites');
@@ -48,7 +36,7 @@ module.exports = function(app,db) {
     });
   }
   
-  function validateURL(url) {
+  function validateUrl(url) {
     // Regex from https://gist.github.com/dperini/729294
     var regex = /^(?:(?:https?|ftp):\/\/)(?:\S+(?::\S*)?@)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,}))\.?)(?::\d{2,5})?(?:[/?#]\S*)?$/i;
     return regex.test(url);
