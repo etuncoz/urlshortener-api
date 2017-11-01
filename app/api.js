7cd45a20adc30be8dbed915dd993e6e34191cfc9
Glitch (brick-board) <none>
2017-11-01 10:38:25 +0000

    Checkpoint

--- app/api.js
+++ app/api.js
@@ -28,4 +28,28 @@ module.exports = function(app,db) {
   var addNewUrl = function () {
     
   }
+  
+  
+  function getUrl(link, db, res) {
+    // Check to see if the site is already there
+    var sites = db.collection('sites');
+    // get the url
+    sites.findOne({
+      "short_url": link
+    }, function(err, result) {
+      if (err) throw err;
+      // object of the url
+      if (result) {
+        // we have a result
+        console.log('Found ' + result);
+        console.log('Redirecting to: ' + result.original_url);
+        res.redirect(result.original_url);
+      } else {
+        // we don't
+        res.send({
+        "error": "This url is not on the database."
+      });
+      }
+    });
+  }
 }
